007c54f84baad94dfcf38b02beb47655a90ba4f8
Omkvist <50019202+Omkvist@users.noreply.github.com>
2019-08-22 11:33:20 +0200

    Add project files.

---
+++ WpfAppIntroduktion/BIZ/ClassBIZ.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfAppIntroduktion.BIZ
+{
+    class ClassBIZ
+    {
+        public ClassBIZ()
+        {
+
+        }
+
+        public void Delopgave12(ListBox listBox)
+        {
+            listBox.Items.Clear();
+            string infoText = "Opgave 1.2: \nEn funktion der udskriver \ntallene fra 4711 til 4736.\n---------------------------";
+            listBox.Items.Add(infoText.ToString());
+            for (int i = 4711; i < 4737; i++)
+            {
+                listBox.Items.Add(i.ToString());
+            }
+        }
+
+        public void Delopgave13(ListBox listBox)
+        {
+            listBox.Items.Clear();
+            string infoText = "Opgave 1.3: \nEn metode der opretter \n25 tilfældige tal \nmellem 100.000 og 1.000.000.\n---------------------------";
+            listBox.Items.Add(infoText.ToString());
+            Random random = new Random(); 
+
+            for (int i = 0; i < 25; i++)
+            {
+                int randomTal = random.Next(100000, 1000001);
+                listBox.Items.Add(randomTal.ToString());
+            }
+        }
+
+        public void Delopgave14(ListBox listBox)
+        {
+            listBox.Items.Clear();
+            string infoText = "Opgave 1.4: \nEn metode der opretter \n25 tilfældige tal \nog sortere dem i stigende orden.\n---------------------------";
+            listBox.Items.Add(infoText.ToString());
+            Random random = new Random();
+            List<int> listInt = new List<int>();
+
+            for (int i = 0; i < 25; i++)
+            {
+                int randomTal = random.Next(1,26);
+                listInt.Add(randomTal);
+            }
+
+            listInt.Sort();
+
+            foreach(int tal in listInt)
+            {
+                listBox.Items.Add(tal.ToString());
+            }
+        }
+
+        public void Delopgave15(ListBox listBox)
+        {
+            listBox.Items.Clear();
+            string infoText = "Opgave 1.5: \nEn metode der opretter \n25 tilfældige tal \nUdskriver tal både som de er, samt sorteret.\n---------------------------";
+            listBox.Items.Add(infoText.ToString());
+            Random random = new Random();
+            List<int> listInt1 = new List<int>();
+            List<int> listInt2 = new List<int>();
+
+            for (int i = 0; i < 25; i++)
+            {
+                int randomTal = random.Next(1, 26);
+                listInt1.Add(randomTal);
+                listInt2.Add(randomTal);
+            }
+
+            listInt2.Sort();
+
+            for(int i = 0; i < listInt1.Count; i++)
+            {
+                listBox.Items.Add($" {listInt1[i]}  --- {listInt2[i]} ");
+            }
+        }
+    }
+}
---
+++ WpfAppIntroduktion/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppIntroduktion.BIZ;
+
+namespace WpfAppIntroduktion
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ClassBIZ classBIZ = new ClassBIZ();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOpg12_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.Delopgave12(ListBoxRes);
+        }
+
+        private void ButtonOpg13_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.Delopgave13(ListBoxRes);
+        }
+
+        private void ButtonOpg14_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.Delopgave14(ListBoxRes);
+        }
+
+        private void ButtonOpg14_Click_1(object sender, RoutedEventArgs e)
+        {
+            classBIZ.Delopgave14(ListBoxRes);
+        }
+
+        private void ButtonOpg15_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.Delopgave15(ListBoxRes);
+        }
+    }
+}

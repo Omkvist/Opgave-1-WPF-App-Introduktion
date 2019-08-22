a2facf66f91f6b8375f1cd523a2333827a7f2be4
Omkvist <50019202+Omkvist@users.noreply.github.com>
2019-08-22 14:28:52 +0200

    Test 2

--- WpfAppIntroduktion/BIZ/ClassBIZ.cs
+++ WpfAppIntroduktion/BIZ/ClassBIZ.cs
@@ -11,7 +11,7 @@ namespace WpfAppIntroduktion.BIZ
     {
         public ClassBIZ()
         {
-            // TEST Constructor
+            // TEST Constructor hej1
         }
 
         public void Delopgave12(ListBox listBox)
